05c722d87d4bafb6529e84a2df034d1cfe52bc32
ravaan-code <biswajitmondal198823@gmail.com>
2025-06-24 08:53:02 +0530

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -4,3 +4,12 @@ if num % 2 != 0:
     print(f"{num} is an odd number")
 else:
     print(f"{num} is not an odd number")
+
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
